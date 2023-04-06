5c5967bbb5b512c7f2c16d5e81acadf9dbc41e5c
bennoloeffler <benno.loeffler@gmx.de>
2023-04-06 14:53:34 +0200

    Initial commit

---
+++ src/belib/core.clj
@@ -0,0 +1,6 @@
+(ns belib.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/belib/core_test.clj
@@ -0,0 +1,7 @@
+(ns belib.core-test
+  (:require [clojure.test :refer :all]
+            [belib.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
